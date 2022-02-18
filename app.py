f652972b67aa09c0b2b4703f4ecb4436d178ae5b
Jordan Casoli <casolijordan@gmail.com>
2022-02-18 15:08:21 -0800

    revert to original

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
-from dash import Dash, html, dcc, Input, Output
 import altair as alt
+from dash import Dash, dcc, html, Input, Output
 from vega_datasets import data
 
-# Read in global data
+    
 cars = data.cars()
 
-# Setup app and layout/frontend
-app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+def plot_altair(xmax):
+    chart = alt.Chart(cars[cars['Horsepower'] < xmax]).mark_point().encode(
+        x='Horsepower',
+        y='Weight_in_lbs')
+    return chart.to_html()
+
+app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
 
 app.layout = html.Div([
-    html.Iframe(
-        id='scatter',
-        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-    dcc.Dropdown(
-        id='origin-widget',
-        value='USA',  # REQUIRED to show the plot on the first page load
-        options=['USA', 'Europe', 'Japan'])])
-
-# Set up callbacks/backend
+        html.Iframe(
+            id='scatter',
+            srcDoc=plot_altair(xmax=0),
+            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+        dcc.Slider(id='xslider', min=0, max=240)])
+        
 @app.callback(
     Output('scatter', 'srcDoc'),
-    Input('origin-widget', 'value'))
-def plot_altair(origin):
-    chart = alt.Chart(cars[cars["Origin"] == origin]).mark_point().encode(
-        x='Horsepower',
-        y='Miles_per_Gallon',
-        tooltip='Horsepower').interactive()
-    return chart.to_html()
+    Input('xslider', 'value'))
+def update_output(xmax):
+    return plot_altair(xmax)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
