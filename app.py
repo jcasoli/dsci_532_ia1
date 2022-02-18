55854b03a93fe43a48d393fcb1669424e9dada00
Jordan Casoli <casolijordan@gmail.com>
2022-02-18 15:02:13 -0800

    get ready for heroku

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ cars = data.cars()
 
 # Setup app and layout/frontend
 app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
+
 app.layout = html.Div([
     html.Iframe(
         id='scatter',
